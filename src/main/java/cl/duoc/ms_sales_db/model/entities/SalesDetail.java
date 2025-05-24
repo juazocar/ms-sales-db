ecbd89dd5928ead74aad65fa004ee8c7a8a17dc5
juazocar <juazocar@profesor.duoc.cl>
2025-05-24 13:53:54 -0400

    se elimina query

--- src/main/java/cl/duoc/ms_sales_db/model/entities/SalesDetail.java
+++ src/main/java/cl/duoc/ms_sales_db/model/entities/SalesDetail.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "sales")
+@Table(name = "sales_detail")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
--- src/main/java/cl/duoc/ms_sales_db/model/repository/SalesDetailRepository.java
+++ src/main/java/cl/duoc/ms_sales_db/model/repository/SalesDetailRepository.java
@@ -3,7 +3,6 @@ package cl.duoc.ms_sales_db.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cl.duoc.ms_sales_db.model.entities.SalesDetail;
@@ -11,9 +10,6 @@ import cl.duoc.ms_sales_db.model.entities.SalesDetail;
 
 @Repository
 public interface SalesDetailRepository extends JpaRepository<SalesDetail, Long>{
-    @Query(value = "SELECT dt.id as id, dt.product_id as product_id, "+
-                   "      dt.quantity as quantity, dt.sales_id as sales_id "+
-                   "FROM sales_detail dt "+
-                   "WHERE dt.sales_id = ?1", nativeQuery = true)
+
     public List<SalesDetail> findBySalesId(Long salesId);
 }
